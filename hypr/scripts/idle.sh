fc0f51c3c9365e5e4f0e52ece736dad85eb21905
Andrii Shekhovtsov <zzzadey@gmail.com>
2024-02-20 14:14:31 +0100

    Add groups, fix idle commands

--- hypr/scripts/idle.sh
+++ hypr/scripts/idle.sh
@@ -2,5 +2,4 @@ lock_cmd="~/.config/hypr/scripts/lock.sh"
 
 swayidle -w timeout 300 "$lock_cmd" \
             timeout 310 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            before-sleep "$lock_cmd" &
+            resume 'hyprctl dispatch dpms on' &
